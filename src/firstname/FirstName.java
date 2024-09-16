3f148163ab08844f3b65f5c436b9b7755038d5e6
nisha <nisha@10.48.56.174>
2024-09-16 12:50:35 -0400

    I aaded a new comment

---
+++ src/firstname/FirstName.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package firstname;
+
+/**
+ *
+ * @author nisha
+ */
+public class FirstName {
+
+    /**
+     * @param args the command line arguments
+     */
+    //added cmments
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello");
+    }
+    
+}
+
+
+
+
+
+
+[user] 
+        
